8f92d7e436be92c3c9f9d506237b9cba0f5dd33e
Pablo Rodríguez <prodrigu75@gmail.com>
2021-09-19 11:55:19 -0300

    Added Forms component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@
 // import { Counter } from "./components/Counter";
 // import { CounterWithHook } from "./components/CounterWithHook";
 // import { Login } from "./components/Login";
+// import { Users } from "./components/Users";
 
-import { Users } from "./components/Users";
+import { Forms } from "./components/Forms";
 
 const App = () => {
   return (
@@ -18,7 +19,8 @@ const App = () => {
       {/* <Counter/> */}
       {/* <CounterWithHook/> */}
       {/* <Login/> */}
-      <Users/>
+      {/* <Users/> */}
+      <Forms/>
     </div>
   )
 }
